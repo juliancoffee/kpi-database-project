cf0c5cb856c3e342b6092c9b339e93f4bac0341c
juliancoffee <lightdarkdaughter@gmail.com>
2020-04-27 16:06:19 +0300

    populate

--- create.sql
+++ create.sql
@@ -1,18 +1,18 @@
-CREATE TABLE Distribution (
+CREATE TABLE Distributions (
     distro_id INTEGER
+    , distro_name VARCHAR(50)
     , os_family VARCHAR(50)
     , development_status VARCHAR(30)
-    , distro_name VARCHAR(50)
     , country_origin VARCHAR(500)
     , average_rating VARCHAR(10)
 );
 
-CREATE TABLE Desktop (
+CREATE TABLE Desktops (
     distro_id INTEGER
     , desktop_name VARCHAR(255)
 );
 
-CREATE TABLE Architecture (
+CREATE TABLE Architectures (
     distro_id INTEGER
     , architecture_name VARCHAR(20)
 );
@@ -22,9 +22,9 @@ CREATE TABLE DistributionKinds (
     , kind_name VARCHAR(50)
 );
 
-CREATE TABLE DistroInteraction (
+CREATE TABLE DistroInteractions (
     parent_id INTEGER
-    , child_int INTEGER
+    , child_id INTEGER
     , parent_kind_name VARCHAR(50)
     , interaction_type VARCHAR(20)
 );
@@ -38,3 +38,52 @@ CREATE TABLE Hits (
     , per_6month INTEGER
     , per_year INTEGER
 );
+
+
+ALTER TABLE Distributions 
+    ADD CONSTRAINT PK_distributions PRIMARY KEY (distro_id);
+ALTER TABLE DistroInteractions
+    ADD CONSTRAINT PK_distro_interaction PRIMARY KEY (child_id);
+ALTER TABLE DistributionKinds 
+    ADD CONSTRAINT PK_distribution_kinds PRIMARY KEY (distro_id, kind_name);
+ALTER TABLE Hits 
+    ADD CONSTRAINT PK_hits PRIMARY KEY (distro_id);
+ALTER TABLE Desktops
+    ADD CONSTRAINT PK_hits PRIMARY KEY (distro_id, desktop_name);
+ALTER TABLE Architectures
+    ADD CONSTRAINT PK_architecture PRIMARY KEY (distro_id, architecture_name);
+
+ALTER TABLE Desktops
+ADD CONSTRAINT
+    FOREIGN KEY (distro_id)
+    REFERENCES Distributions (distro_id);
+
+ALTER TABLE Architectures
+ADD CONSTRAINT FK_architectures_distro
+    FOREIGN KEY (distro_id)
+    REFERENCES Distributions (distro_id);
+
+ALTER TABLE DistributionKinds
+ADD CONSTRAINT FK_kind_distribution
+    FOREIGN KEY (distro_id)
+    REFERENCES Distributions (distro_id);
+
+ALTER TABLE DistroInteractions
+ADD CONSTRAINT FK_parent_distro
+    FOREIGN KEY (parent_id)
+    REFERENCES Distributions (distro_id);
+
+ALTER TABLE DistroInteractions
+ADD CONSTRAINT FK_child_distro
+    FOREIGN KEY (child_id)
+    REFERENCES Distributions (distro_id);
+    
+ALTER TABLE DistroInteractions
+ADD CONSTRAINT FK_parentkind_name
+    FOREIGN KEY (parent_kind_name)
+    REFERENCES DistributionKinds (kind_name);
+
+ALTER TABLE Hits
+ADD CONSTRAINT FK_distro
+    FOREIGN KEY (distro_id)
+    REFERENCES Distributions (distro_id);
